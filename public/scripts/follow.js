31830e5104a4c1a68ac7479ba8077166abdf9610
Madison Wong <madiwong@mit.edu>
2022-10-14 23:59:22 -0400

    follower concept

--- public/scripts/follow.js
+++ public/scripts/follow.js
@@ -1,8 +1,5 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-
 /**
  * Fields is an object mapping the names of the form inputs to the values typed in
- * e.g. for createUser, fields has properites 'username' and 'password'
  */
 
 function followUser(fields) {
